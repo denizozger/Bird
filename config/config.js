853115bf14af753e42d97fa518083116fdb17795
Deniz Ozger <denizozger@gmail.com>
2014-09-24 17:31:31 +0100

    removed config from gitignore

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = function() {
+
+  this.cdn = {
+    provider: process.env.CDN_PROVIDER,
+    username: process.env.CDN_USERNAME,
+    apiKey: process.env.CDN_API_KEY,
+    region: process.env.CDN_REGIION,
+    containerName: process.env.CDN_CONTAINER_NAME
+  }
+
+  this.ftp = {
+    host: process.env.FTP_HOST,
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    directory: process.env.FTP_DIRECTORY,
+    filePermission: 644
+  }
+
+  this.auth = {
+    allowedDomain: 'gmail.com'
+  }
+
+  return this;
+}
